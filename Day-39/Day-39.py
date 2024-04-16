fc0cfe7e4feabca05c1dceeba5043abcc30bb8dc
chaupmbn <chaupmbn>
2024-04-16 13:39:30 +0700

    upload homeworks

---
+++ Day-39/Day-39.py
@@ -0,0 +1,13 @@
+def is_prime(n: int) -> bool:
+    if n <= 1:
+        return False
+    else:
+        for i in range(2,int(n**0.5+1)):
+            if n%i == 0:
+                return False
+                break
+        return True
+print(is_prime(2))
+print(is_prime(10))
+print(is_prime(432))
+print(is_prime(13))
